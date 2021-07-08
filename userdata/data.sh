77589ebd0a432a4f086ef513582875394a0f3445
RX1208 <iliucoco@163.com>
2021-07-08 14:22:31 +0000

    hello

--- userdata/data.sh
+++ userdata/data.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-yum update -y
-amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-yum install -y httpd mariadb-server
+
+
+#!/bin/bash -xe
+yum install -y httpd php
 systemctl start httpd
 systemctl enable httpd
 usermod -a -G apache ec2-user
@@ -9,4 +9,4 @@ chown -R ec2-user:apache /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
-echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+echo "<?php phpinfo(); ?>" > /var/www/html/index.php
